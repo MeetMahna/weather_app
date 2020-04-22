879bfe50f1d10382a8d7491b8b5b3a45cf5d7f54
skullcandy69 <iammeet50@gmail.com>
2020-04-22 21:42:24 +0530

    Revert "Initial commit"
    This reverts commit 8f40400d68f68d0cdb48d7b5bc90b9f973a40930.

--- weather/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, TextInput
-from .models import Place
-
-
-class CityForm(ModelForm):
-    class Meta:
-        model = Place
-        fields = ['name']
-        widgets = {'name': TextInput(attrs={'class': 'input', 'placeholder': 'City Name', 'autocomplete': 'off'})}
